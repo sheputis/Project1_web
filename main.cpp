aea0f678e27a5c0fda04e2a5be03ad17b95ac301
sheputis <arnoldas@student.matnat.uio.no>
2015-09-15 14:44:00 +0200

    code added to solve problem C)

--- main.cpp
+++ main.cpp
@@ -2,37 +2,50 @@ using namespace std;
 #include <iostream>
 #include <cmath>
 #include <fstream>
+#include <algorithm> //to find max value in error list Prob C)
 
-
+//for PROBLEM B)
+void initialize(int, double *, double *,double *);
 void solve_matrix(int, double *, double *);
 void write_out(int,double *, double *, double *);
+//for PROBLEM C)
+void relative_error(int,double *,double *);
+
 int main()
 {
-    int n = 10;
-    double h = 1./(n+1);
 
-
-
+    // PROBLEM B)
+    int n = 100000;
     // making pointers
     double *u;
     double *x;double *f;double *u_closed_form;
     // giving size to the arrays
     f = new double[n];u_closed_form = new double[n];x=new double[n];u = new double[n];
     // initializing vectors
-    for(int i=0;i<n;i++){
-        f[i]=100.*exp(-10.*i*h)*h*h;
-        u_closed_form[i]=1-(1-exp(-10))*h*i-exp(-10*h*i);
-        x[i]=i*h;
-    }
+    initialize(n,f,u_closed_form,x);
 
     // solving the matrix
     solve_matrix(n, u,f);
+    // writing out the results to files so that we can plot them later
     write_out(n,x,u,u_closed_form);
+
+    // PROBLEM C)
+
+    relative_error(n,u_closed_form,u);
+
 
     return 0;
 }
 
 
+void initialize(int n_,double *f_, double *u_closed_form_,double *x_){
+    double h = 1./(n_+1);
+    for(int i=0;i<n_;i++){
+        f_[i]=100.*exp(-10.*i*h)*h*h;
+        u_closed_form_[i]=1-(1-exp(-10))*h*i-exp(-10*h*i);
+        x_[i]=i*h;
+    }
+}
 
 void solve_matrix(int n_,double *u_,double *f_){
 
@@ -70,3 +83,19 @@ void write_out(int n_,double *x_,double *u_,double *u_closed_form_){
     outputFile1.close();
     outputFile2.close();
 }
+
+
+void relative_error(int n_,double *u_closed_form_, double *u_){
+
+    double errors[n_];
+    for(int i=1;i<n_;i++){
+        errors[i]=abs((u_[i]-u_closed_form_[i])/u_closed_form_[i]);
+        //cout<<errors[i];
+
+    }
+    double max=*std::max_element(errors,errors+n_);
+    cout << "problem C) for N="<<n_<<" the relative error is: "<< max<<endl;
+
+}
+
+
